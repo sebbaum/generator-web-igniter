d38ede26b128eb521eb47f20605d5b8de5bf3032
Sebastian Baum <sebastian.baum@ion2s.com>
2017-12-05 11:12:18 +0100

    - fixed linting errors

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,16 +6,20 @@ const yosay = require('yosay');
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the ultimate ' + chalk.red('generator-web-igniter') + ' generator!'
-    ));
+    this.log(
+      yosay(
+        'Welcome to the ultimate ' + chalk.red('generator-web-igniter') + ' generator!'
+      )
+    );
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    const prompts = [
+      {
+        type: 'confirm',
+        name: 'someAnswer',
+        message: 'Would you like to enable this option?',
+        default: true
+      }
+    ];
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
